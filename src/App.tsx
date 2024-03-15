737c3ad0562cd4858f7dde436914d17fa64b2ee3
Rich Wareham <rich@richwareham.com>
2024-03-15 11:09:11 +0000

    feat: add basic decode data example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+const decodeAudioData = (audioCtx: BaseAudioContext, buffer: ArrayBuffer) => (
+  new Promise((resolve, reject) => audioCtx.decodeAudioData(buffer, resolve, reject))
+);
+
+const doIt = async () => {
+  const audioCtx = new AudioContext();
+  const response = await fetch("audio/nonaspr-cnuts-song-all.mp3");
+
+  if (!response.ok) {
+    throw new Error(`Error fetching audio data: ${response.statusText}`);
+  }
+
+  const decodedData = await decodeAudioData(audioCtx, await response.arrayBuffer());
+  console.log(decodedData);
+};
+
 const App = () => {
-  return <>
-    <div className="text-3xl font-bold underline">Hello</div>
-  </>;
+  return <div className="container mx-auto my-4">
+    <button onClick={() => { doIt(); }}>do it</button>
+  </div>;
 };
 
 export default App;
