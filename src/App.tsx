c1dacc74cc93a6d4578ba158b17c30317fcc7e0e
Rich Wareham <rich@richwareham.com>
2024-03-15 11:19:51 +0000

    feat: port to Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
-const decodeAudioData = (audioCtx: BaseAudioContext, buffer: ArrayBuffer) => (
-  new Promise((resolve, reject) => audioCtx.decodeAudioData(buffer, resolve, reject))
-);
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+const decodeAudioData = (buffer: ArrayBuffer, audioCtx?: BaseAudioContext) =>
+  new Promise((resolve, reject) =>
+    (audioCtx ?? new AudioContext()).decodeAudioData(buffer, resolve, reject),
+  );
 
 const doIt = async () => {
-  const audioCtx = new AudioContext();
   const response = await fetch("audio/nonaspr-cnuts-song-all.mp3");
 
   if (!response.ok) {
     throw new Error(`Error fetching audio data: ${response.statusText}`);
   }
 
-  const decodedData = await decodeAudioData(audioCtx, await response.arrayBuffer());
+  const decodedData = await decodeAudioData(await response.arrayBuffer());
   console.log(decodedData);
 };
 
 const App = () => {
-  return <div className="container mx-auto my-4">
-    <button onClick={() => { doIt(); }}>do it</button>
-  </div>;
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          WebAudio Experiments
+        </Typography>
+        <Button
+          variant="contained"
+          size="large"
+          onClick={() => {
+            doIt();
+          }}
+        >
+          Load audio
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import App from "./App";
+import theme from "./theme";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
